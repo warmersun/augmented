05d4f9f8fd58456b1ff6452f1ce4a58d49ae6844
Warmer Sun <tamas.simon@warmersun.com>
2024-06-19 02:20:33 +0000

    save

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+from typing_extensions import override
 import chainlit as cl
-from openai import OpenAI
+from openai import OpenAI, AssistantEventHandler
 from openai.types.beta import Assistant
+from openai.types.beta.threads import Text, TextDelta
 
 from augmented import Worker
 
+class EventHandler(AssistantEventHandler):
+    @override
+    def on_text_created(self, text: Text) -> None:
+        pass
+
+    @override
+    def on_text_delta(self, delta: TextDelta, snapshot: Text) -> None:
+        pass
 
 @cl.on_chat_start
 async def start():
@@ -12,19 +22,95 @@ async def start():
     ) 
     assistant =  client.beta.assistants.create(
       name="Content Worker",
-      instructions="You are a content creator assistant. You work together with the user to create lesson content for the learning goals specified in the input. Input: learning goals. Output: content for a lesson plan.",
+      instructions=
+        "You are a content creator assistant." \
+        "You work together with the user - the facilitator - to create lesson content for the learning goals specified in the input. " \
+        "Make sure the content is fun, interesting for students who are high school juniors."
+        "Input: learning goals. " \
+        "Output: content for a each learning goal that the facilitator can later use to build out their lesson plan. " \
+        "When you think you're done and have everything to create the output remind the user to say 'FINISHED'.",
       model="gpt-4o",
     )
-    cl.user_session.set("worker", Worker(assistant, "Learning goal: Russian civil war"))
+    worker =  Worker(assistant, EventHandler(), "Learning goal: Great October Revolution in 1917. The student should know about Lenin, the tsar and why peoplpe revolted, just the basics.")
+    cl.user_session.set("worker",worker)
+    # the AI starts
+    await cl.Message(content=worker.get_next_assistant_message()).send()
 
 @cl.on_message
 async def main(message: cl.Message):
+    # the user has just submitted a message...     
+    # so what do we do?
+    # we need to get the worker from the user session
+    # then we let the worker know about the message
+    # then we check if this message means that the user has finished and the job is completed
+    # if so, we ask the worker to generate the output and show it to the user to confirm
+    # if not, we ask the worker to continue and return the next AI message to show 
+    # NOTE: this will be more complicated with function calls, tool use, streaming
+
+    # get the worker from the user session
     worker = cl.user_session.get("worker")
-    if worker:
-        # Send a response back to the user
-        await cl.Message(
-            content=worker.next_step(message.content)
-        ).send()
+    if worker is not None:
+        # let the worker know about the message
+        worker.submit_user_message(message.content)
+        #  check if this message means that the user has finished and the job is completed
+        if worker.is_finished():
+            # we ask the worker to generate the output and show it to the user to confirm
+            output = worker.generate_output()
+            actions = [
+                # confirm
+                cl.Action(
+                    name='confirm_output', 
+                    value="confirm", 
+                    label="Confirm", 
+                    description="Confirm the output is good to go."
+                ),
+                # cancel
+                cl.Action(
+                    name='confirm_output', 
+                    value="cancel", 
+                    label="Cancel", 
+                    description="Need to keep working on the output."
+                )
+            ]
+            cl.user_session.set("actions", actions)
+            elements = [
+                cl.Text(name="output", content=output, display="inline")
+            ]
+            await cl.Message(
+                content="Confirm the output is good to go!", 
+                actions=actions,
+                elements=elements
+            ).send()
+        else:
+            await cl.Message(
+                content=worker.get_next_assistant_message()                
+            ).send()
+
+@cl.action_callback("confirm_output")
+async def confirm_output(action: cl.Action):
+    # after the user or the AI has decided that the job is complete the AI generated the output and presented it for the user.
+    # the user was asked to confirm that the output is good to go
+    # we need to let the worker know whether the output is good to go now and thejob is finished or that they need to keep working on it
+
+    # get the worker from the user session
+    worker = cl.user_session.get("worker")
+    if worker is not None:
+        # let the worker know 
+        if action.value == "confirm":              
+            worker.output_is_good_to_go()
+        else:        
+            worker.output_needs_work()
+            # we ask the worker to continue and return the next AI message to show
+            await cl.Message(
+                content=worker.get_next_assistant_message()
+            ).send()
+        # remove the action buttons from the UI
+        actions = cl.user_session.get("actions")
+        if actions is not None:
+            for action in actions:
+                await action.remove()
+
+    return "Confirmed!"
 
 if __name__ == "__main__":
   from chainlit.cli import run_chainlit
--- augmented/worker.py
+++ augmented/worker.py
@@ -1,10 +1,14 @@
-from openai import OpenAI
+import json
+
+from openai import OpenAI, AssistantEventHandler
 from openai.types.beta import Assistant
 from openai.types.beta.threads import Message
 
+
 class Worker:
-  def __init__(self, assistant: Assistant, input: str) -> None:
+  def __init__(self, assistant: Assistant, event_handler: AssistantEventHandler, input: str) -> None:
     self.assistant = assistant
+    self.event_handler = event_handler
     self.additional_instructions = f"The input is: {input}"
 
     self.client = OpenAI(
@@ -12,24 +16,63 @@ class Worker:
     ) 
     self.thread = self.client.beta.threads.create()
     self.run = None
+    self.finished = False
+    self.output = None
+    self.output_confirmed = False
 
-  def __submit_user_message(self, msg: str) -> Message:
+  def submit_user_message(self, msg: str) -> None:
     message = self.client.beta.threads.messages.create(
       thread_id=self.thread.id,
       role = "user",
       content = msg
     )
-    return message
-  
-  def next_step(self, prompt: str) -> str:
-    message = self.__submit_user_message(prompt)
+    self.check_if_we_finished(msg)
 
+  def check_if_we_finished(self, msg: str) -> None:
+    self.finished = msg.upper().endswith("FINISHED")
+  
+  def is_finished(self) -> bool:
+    return self.finished
+
+  def generate_output(self) -> str:
+    output_run =  self.client.beta.threads.runs.create_and_poll(
+      thread_id=self.thread.id,
+      assistant_id=self.assistant.id,
+      # TODO: support multiple outputs
+      additional_instructions=
+        "Produce the output in JSON format, as a JSON structure with a single string"
+        "called `output`.\n" + self.additional_instructions,
+      response_format={"type": "json_object"}
+    )
+    if output_run.status == 'completed':
+      messages = self.client.beta.threads.messages.list(
+        thread_id=self.thread.id,
+        run_id = output_run.id
+      )
+      # Check if there are messages in the response
+      if messages.data:
+        for message in messages:
+          # Access the text content of each message
+          if message.content[0].type == 'text':
+            output_text = message.content[0].text.value
+            self.output = json.loads(output_text).get("output")
+            return self.output
+          else:
+            # TODO: fix
+            return "n/a"
+      else:
+        # TODO: fix
+        return "No messages found."
+      
+    # TODO: fix    
+    return output_run.status
+  
+  def get_next_assistant_message(self) -> str:
     self.run = self.client.beta.threads.runs.create_and_poll(
       thread_id=self.thread.id,
       assistant_id=self.assistant.id,
       additional_instructions=self.additional_instructions
     )
-
     if self.run.status == 'completed':
       messages = self.client.beta.threads.messages.list(
         thread_id=self.thread.id,
@@ -37,17 +80,31 @@ class Worker:
       )
       # Check if there are messages in the response
       if messages.data:
-          for message in messages:
-              # Access the text content of each message
-              if message.content[0].type == 'text':
-                message_text = message.content[0].text.value
-              else:
-                message_text = "n/a"
-              return message_text
+        for message in messages:
+          # Access the text content of each message
+          if message.content[0].type == 'text':
+            message_text = message.content[0].text.value
+          else:
+            message_text = "n/a"
+          self.check_if_we_finished(message_text)
+          return message_text
       else:
-          return "No messages found."
-
-
-    
+        # TODO: fix
+        return "No messages found."
+    # TODO: fix    
     return self.run.status
+  
+  def output_is_good_to_go(self) -> None:
+    self.output_confirmed = True
+
+  def output_needs_work(self) -> None:
+    self.output_confirmed = False
+    self.finished = False
+    # need to send the AI a message that the output needs work
+    self.submit_user_message(
+      f"The output needs work." \
+      f"This is the output that was generated but not good to go:\n***\n{self.output}"
+    )
+  
+
     
